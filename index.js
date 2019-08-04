300e9db9e835abe6c158f2d43c0c8fa7be2576a7
Evan  Markert <evanmarkert@gmail.com>
2019-08-04 13:39:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+
+// returns all drivers matching passed in name, case insensitive, returns empty array if no matches
+function findMatching(array, name) {
+  return array.filter(function (driverName) {
+    if (driverName.toLowerCase() === name.toLowerCase())(return array;)
+  })
+};
+
+function fuzzyMatch() {
+
+}
+
+function matchName() {
+
+}
